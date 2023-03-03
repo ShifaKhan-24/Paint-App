f3654c510b9b82299d97a95344c75a974361a5ae
Shifa Zakir Khan <shifa24khan@gmail.com>
2023-03-03 23:08:32 +0530

    added  draw Rectangle

--- script.js
+++ script.js
@@ -1,8 +1,67 @@
-const canvas = document.getElementById("canvas");
-const board = document.querySelector(".board");
+const canvas = document.getElementById("canvas"),
+  toolsBtn = document.querySelectorAll(".tool"),
+  board = document.querySelector(".board"),
+  ctx = canvas.getContext("2d");
 
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
+// Global variables with default value
+
+let isDrawing = false;
+let selectedTool = "brush";
+let brushWidth = 5;
+let prevMouseX, prevMouseY;
+
+window.addEventListener("load", () => {
+  // set canvas width & height...
+  //offsetwidth/height returns viewable width/height of an element
+
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+});
+
+const drawReact = (e) => {
+  ctx.strokeRect(
+    e.offsetX,
+    e.offsetY,
+    prevMouseX - e.offsetX,
+    prevMouseY - e.offsetY
+  );
+};
+const startDraw = (e) => {
+  isDrawing = true;
+  prevMouseX = e.offsetX; // passing current mouse in X position as prevMouseX value
+  prevMouseY = e.offsetY; //passing current mouse in Y position as prevMouseY value
+  ctx.beginPath(); // creates new path to draw
+  ctx.lineWidth = brushWidth; // passing brushsize as line width
+  // copying canvas data & passing as snapshot value.. this avoids dragging the image
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  // getImageData() returns an imagedata object that copies the pixel data
+};
+const drawing = (e) => {
+  if (!isDrawing) return; // if isDrawing is false
+  ctx.putImageData(snapshot, 0, 0); // adding copied canvas data onto this canvas
+
+  if (selectedTool === "brush") {
+    ctx.lineTo(e.offsetX, e.offsetY); // creating line accd to mouse pointer
+    ctx.stroke(); // drawing/filling line with color
+  } else if (selectedTool === "rectangle") {
+    drawReact(e);
+  }
+};
+
+toolsBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".options .active").classList.remove("active");
+    btn.classList.add("active");
+    selectedTool = btn.id;
+    console.log(selectedTool);
+  });
+});
+
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
 
 var theColor = "";
 var lineW = 5;
